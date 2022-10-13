eecd2f8233d4b9902d7756012aa9be83b5f0e53c
Honeydropjahbless <honeydropjahbless@gmail.com>
2022-10-13 08:02:09 -0700

    Advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <unisted.h>
 
 /**
- * main - Prints a text
- *
- * Return: Always 1(Success)
+ * main - Entry point
+ * Return: Always 1
  */
+
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
