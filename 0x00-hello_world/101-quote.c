2a5ac955c22235260c017246ba9f6252efe9a9df
Honeydropjahbless <honeydropjahbless@gmail.com>
2022-10-13 07:19:57 -0700

    Task-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * Return: Always 1.
  */
 int main(void)
 {
