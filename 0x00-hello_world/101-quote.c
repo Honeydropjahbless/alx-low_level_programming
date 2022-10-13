a15a7a42cbe1444e6de037721422053ac12d0352
Honeydropjahbless <honeydropjahbless@gmail.com>
2022-10-13 07:31:20 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: Always 1.
+ * main - Entry point
+ * Return: Always 1
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
