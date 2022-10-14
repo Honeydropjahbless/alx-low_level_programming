23b7c7e6d3e7b401dbdc9a35de09d0fd1fce6f4c
Honeydropjahbless <honeydropjahbless@gmail.com>
2022-10-14 13:16:37 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '48'; c <= '57'; c++)
+	{
+		putchar(c);
+		if (c != '57');
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
