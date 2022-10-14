b72361508372e8929ad59b8d18b162bf89b116dd
Honeydropjahbless <honeydropjahbless@gmail.com>
2022-10-14 13:03:50 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,17 @@
  */
 int main(void)
 {
-	int d;
-	char low;
+	char c;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-	printf('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
 
